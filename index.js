6efaef476cb881b8735daa5acba85f86fc16c639
Rafael Amaral <rafael.amaral-ext@betchavas.com>
2022-04-04 13:59:22 -0300

    first API Netflix

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+const filmes = require('./src/data/filmes.json')
+
+server.get('/filmes', (req, resp) => {
+    return resp.json(filmes);
+}); 
+
+server.listen(3000, () => {
+    console.log('Server running...');
+})
